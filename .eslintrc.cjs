18c6b9d1ec4ae72c50e91c661a2cf198139287fc
Judit Langer <judit.langer@aon.at>
2023-11-09 15:59:00 +0100

    Add eslint rule for no floating promises

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 const config = {
   extends: ['upleveled'],
+
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error',
+  },
 };
 
 module.exports = config;
