4018b329808d5a58c870381d2dc1735c1b866e83
Judit Langer <judit.langer@aon.at>
2023-11-21 08:04:45 +0100

    Add avatar and last message to response

--- controllers/conversations.ts
+++ controllers/conversations.ts
@@ -40,6 +40,7 @@ export const getConversationsByUserId = async (
           // @ts-ignore
           id: true,
           firstName: true,
+          avatar: true,
         },
       },
       participant: {
@@ -47,6 +48,17 @@ export const getConversationsByUserId = async (
           // @ts-ignore
           id: true,
           firstName: true,
+          avatar: true,
+        },
+      },
+      message: {
+        orderBy: {
+          createDate: 'desc',
+        },
+        take: 1,
+        select: {
+          text: true,
+          createDate: true,
         },
       },
     },
