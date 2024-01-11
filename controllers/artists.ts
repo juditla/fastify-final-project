24801a2d11d941b25c1b314bacc30979c2d852bd
Judit Langer <judit.langer@aon.at>
2024-01-11 12:40:10 +0100

    Delete unnecessary condition in addArtistRating

--- controllers/artists.ts
+++ controllers/artists.ts
@@ -300,7 +300,7 @@ export const addArtistRating = async (
       },
     });
 
-    if (artist && newArtistRating) {
+    if (artist) {
       const artistRating = await prisma.artistRating.aggregate({
         _avg: {
           rating: true,
